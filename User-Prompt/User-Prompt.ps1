d2a24a8c74cf06d1e2ee44dfdb45b4bcd3216389
tankcr <kristopher.roy@gmail.com>
2017-10-06 12:41:23 -0700

    Change
    Updated Script to allow for any two options

--- User-Prompt/User-Prompt.ps1
+++ User-Prompt/User-Prompt.ps1
@@ -2,7 +2,7 @@
 User-Prompt.ps1
 Created By Kristopher Roy
 Date Created 06Oct17
-Script purpose - A Basic Dynamic Yes,No User Prompt Function
+Script purpose - A Basic Dynamic 2 option User Prompt Function
 #>
 
 function user-prompt
@@ -12,23 +12,42 @@ function user-prompt
     [string] $Title,
     [Parameter(Mandatory=$False)]
     [string]$Message,
-    #String to Display what Yes means in a tooltip
-    [Parameter(Mandatory=$False)]
-    [string]$YesToolTip,
-    #String to Display what No means in a tooltip
 	[Parameter(Mandatory=$False)]
-    [string]$NoToolTip
+    [string]$Select1,
+	[Parameter(Mandatory=$False)]
+    [string]$Select2,
+    #String to Display what first option means in a tooltip
+    [Parameter(Mandatory=$False)]
+    [string]$Selection1ToolTip,
+    #String to Display what second option means in a tooltip
+	[Parameter(Mandatory=$False)]
+    [string]$Selection2ToolTip
     )
 
     If($Title -eq $Null -or $Title -eq ""){$title = "Selection"}
-    If($message -eq $Null -or $Message -eq ""){$message = "Basic Yes,No Options"}
-    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
-    $YesToolTip
-    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
-    $NoToolTip
-    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+    If($Select1 -eq $Null -or $Select1 -eq ""){
+			$selection1 = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+			$select1ToolTip
+		}
+	ELSE
+	{
+		$selection1 = New-Object System.Management.Automation.Host.ChoiceDescription "&$Select1", `
+		$select1ToolTip
+	}
+    If($Select2 -eq $Null -or $Select2 -eq "")
+	{
+		$selection2 = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+		$select2ToolTip
+	}
+	ELSE
+	{
+		$selection2 = New-Object System.Management.Automation.Host.ChoiceDescription "&$Select2", `
+		$select2ToolTip
+	}
+    If($message -eq $Null -or $Message -eq ""){$message = "Basic $select1,$select2 Options"}
+	$options = [System.Management.Automation.Host.ChoiceDescription[]]($selection1, $selection2)
     $choice = $host.ui.PromptForChoice($title, $message, $options, 0)    
     $choice = [int]$choice
     Return $choice
-    #Returns Yes = 0, and No = 1
+    #Returns selection1 = 0, and selection2 = 1
 }
